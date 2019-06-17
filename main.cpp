f200ae6acb39da4dd3544bdc39317cccfe9ed393
M Moshahid Ul Islam Kallol <36559535+mkallol@users.noreply.github.com>
2019-06-17 00:03:25 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void toh(int n, int a, int b,int c)
+{
+    if(n>0)
+    {
+      toh(n-1,a,c,b);
+      cout<<"("<<a<<" to " << c<<")"<<endl;
+      toh(n-1,b,a,c);
+      
+    }
+};
+
+int main()
+{
+    toh(4,1,2,3);
+}
